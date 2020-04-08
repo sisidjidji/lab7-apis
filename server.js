4b2dbe6eb3e8af85811f88f97b1f6b5ddb8fa060
sisidjidji <s.azibi@live.fr>
2020-04-08 10:30:23 -0500

    wether API

---
+++ server.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// Load Environment Variables from the .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Application Setup
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors()); // Middleware
+
+
+
+app.get('/weather', weatherHandler) ;
+
+function weatherHandler(request, response) {
+  const weatherData=require('./data/darksky.json');
+  // const weather=[];
+let x= weatherData.daily.data.map( dailyWeather=>{
+    // eslint-disable-next-line semi
+    new Weather(dailyWeathe);
+  });
+  response.send(x);
+}
+
+// Add /location route
+app.get('/location', locationHandler);
+
+// Route Handler
+function locationHandler(request, response) {
+  const city = request.query.city;
+  // const location = new Location(city, geoData);
+  const url='https://us1.locationiq/v1/search.php'
+  superagent.get(url)
+  .query({
+    key:process.env.GEO_KEY,
+    q:city,
+    format:'json'
+  })
+
+  .then(locationResponse=>{
+    let geoData=locationResponse.body;
+    const location = new Location(city,geoData);
+    response.send(location);
+  })
+  .catch(err=>{
+    errorHandler(err,request,response);
+  })
+}
+
+// Has to happen after everything else
+app.use(notFoundHandler);
+// Has to happen after the error might have occurred
+app.use(errorHandler); // Error Middleware
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
+// Helper Functions
+
+function errorHandler(error, request, response, next) {
+  console.log(error);
+  response.status(500).json({
+    error: true,
+    message: error.message,
+  });
+}
+
+function notFoundHandler(request, response) {
+  response.status(404).json({
+    notFound: true,
+  });
+}
+
+function Location(city, geoData) {
+  this.search_query = city; //
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = parseFloat(geoData[0].lat);
+  this.longitude = parseFloat(geoData[0].lon);
+}
+
+function Weather(weatherData){
+  this.forecast = weatherData.summary;
+  this.time = new Date(weatherData.time * 1000);
+}
